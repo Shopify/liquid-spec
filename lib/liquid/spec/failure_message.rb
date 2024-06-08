a85830afc83697a26a21e36f979b71fb7a2f84ac
Ian Ker-Seymer <ian.kerseymer@shopify.com>
2024-06-08 11:08:15 -0400

    Support tldr gem

--- lib/liquid/spec/failure_message.rb
+++ lib/liquid/spec/failure_message.rb
@@ -5,20 +5,30 @@ module Liquid
     class FailureMessage
       attr_reader :spec, :actual, :width
 
-      Differ = Object.new.extend(Minitest::Assertions)
-
       def initialize(spec, actual, width: nil)
         @spec = spec
         @actual = actual
         @width = width
       end
 
+      class << self
+        def rerun_command_for(spec)
+          if defined?(TLDR)
+            "bundle exec tldr test/integration/liquid_spec.rb --name=/#{spec.name}/"
+          else
+            "rake liquid_spec TESTOPTS=\"--name=/#{spec.name}/\""
+          end
+        end
+      end
+
       def to_s
+        return @rendered_message if defined?(@rendered_message)
+
         template = render_box(content: spec.template, name: "Template")
         environment = render_box(content: spec.environment.inspect, name: "Environment")
-        rendered_diff = render_box(content: Differ.diff(spec.expected, actual), name: "Diff")
+        rendered_diff = render_box(content: render_diff(spec.expected, actual), name: "Diff")
 
-        <<~MSG
+        @rendered_message = <<~MSG
           #{template}
 
           #{environment}
@@ -28,11 +38,15 @@ module Liquid
           ===========================
 
           To rerun this spec, run the following command:
-            $ rake liquid_spec TESTOPTS="--name=/#{spec.name}/"
+            $ #{self.class.rerun_command_for(spec)}
         MSG
       end
 
       private
+
+      def render_diff(expected, actual)
+        SuperDiff::EqualityMatchers::Main.call(expected:, actual:)
+      end
 
       def render_box(name:, content:)
         TTY::Box.frame(content, width: width, title: {top_left: name}).strip
