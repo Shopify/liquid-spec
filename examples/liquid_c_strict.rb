9badfd17adc0b13c3dd75b8f65718ef644baa272
Tobi Lutke <tobi@shopify.com>
2026-01-02 15:06:46 -0500

    feat: add liquid_c_strict adapter for strict mode with liquid-c

---
+++ examples/liquid_c_strict.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+#
+# Shopify/liquid with liquid-c extension (strict mode)
+#
+# Run: liquid-spec examples/liquid_c_strict.rb
+#
+# Requires: gem install liquid-c
+#
+
+require "liquid/spec/cli/adapter_dsl"
+
+LiquidSpec.setup do
+  require "liquid"
+  require "liquid/c"
+
+  raise "liquid-c is not available. Install with: gem install liquid-c" unless defined?(Liquid::C)
+  raise "liquid-c is not enabled" unless Liquid::C.enabled
+end
+
+LiquidSpec.configure do |config|
+  config.features = [:core]
+end
+
+LiquidSpec.compile do |source, options|
+  # Force strict mode regardless of spec
+  Liquid::Template.parse(source, error_mode: :strict, **options)
+end
+
+LiquidSpec.render do |template, assigns, options|
+  context = Liquid::Context.build(
+    static_environments: assigns,
+    registers: Liquid::Registers.new(options[:registers] || {}),
+    rethrow_errors: options[:strict_errors],
+  )
+  context.exception_renderer = options[:exception_renderer] if options[:exception_renderer]
+
+  template.render(context)
+end
